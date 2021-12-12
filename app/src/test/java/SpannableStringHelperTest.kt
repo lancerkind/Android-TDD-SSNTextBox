833dd5dac8245f9223b2d07cd8c316054b961f46
Lance Kind <lkind1@ford.com>
2021-12-12 07:18:07 -0600

    Two not well designed classes. We'll get back to these when we tackle color and make them better.

---
+++ app/src/test/java/SpannableStringHelperTest.kt
@@ -0,0 +1,28 @@
+package com.example.texteditbasicactivity
+
+import android.graphics.Color
+import android.text.SpannableStringBuilder
+import mywidgets.SpannableStringHelper
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+
+class SpannableStringHelperTest {
+    lateinit var helper : SpannableStringHelper
+
+    @Before
+    fun before() {
+    }
+
+    @Ignore
+    @Test
+    fun getColor(){
+        /*
+        val string  = SpannableStringBuilder("h")
+
+        helper = SpannableStringHelper(string)
+        assertEquals(Color.BLACK, helper.getColor(0))
+
+         */
+    }
+}
